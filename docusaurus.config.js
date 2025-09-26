d2202900e0a3e4f857dd87c5bf3b784d544353db
Farhan <farhan_abdulla@berkeley.edu>
2025-09-26 14:10:50 -0700

    small fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Fabdulla1", // Usually your GitHub org/user name.
+  organizationName: "Deep Past Initiative", // Usually your GitHub org/user name.
   projectName: "DeepPast", // Usually your repo name.
 
   onBrokenLinks: "throw",
